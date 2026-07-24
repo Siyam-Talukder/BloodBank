03402ca33f54e3cf2e466892a0d97d0214c7ec83
Siyam Talukder <mdsiyamtalukder1@gmail.com>
2026-07-24 20:37:51 +0600

    Final assignment submission

---
+++ Controllers/DonationController.cs
@@ -0,0 +1,58 @@
+﻿using BloodBank.EF;
+using BloodBank.EF.Tables;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBank.Controllers
+{
+
+    public class DonationController : Controller
+    {
+        BloodBankDbContext db;
+        public DonationController(BloodBankDbContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Donors = db.Donors.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Donation donation)
+        {
+            ModelState.Remove("Donor");
+            if (ModelState.IsValid)
+            {
+                db.Donations.Add(donation);
+                var res = db.SaveChanges();
+                if (res > 0)
+                {
+                    return RedirectToAction("List");
+                }
+            }
+            ViewBag.Donors = db.Donors.ToList();
+            return View(donation);
+        }
+
+        public IActionResult List()
+        {
+            var donations = db.Donations.Include(item => item.Donor).ToList();
+            return View(donations);
+        }
+
+        public IActionResult TotalBloodVolume()
+        {
+            var totalVolume = db.Donations.Sum(item => item.VolumeMl);
+            ViewBag.TotalVolume = totalVolume;
+
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/DonorController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using BloodBank.EF;
+using BloodBank.EF.Tables;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBank.Controllers
+{
+    public class DonorController : Controller
+    {
+        BloodBankDbContext db;
+        public DonorController(BloodBankDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Donor d)
+        {
+            if (d.LastDonationDate >= DateOnly.FromDateTime(DateTime.Today))
+            {
+                ModelState.AddModelError("LastDonationDate", "The last donation date must be a past date.");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Donors.Add(d);
+                var res = db.SaveChanges();
+                if (res > 0)
+                {
+                    return RedirectToAction("List");
+                }
+            }
+            return View(d);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var donor = db.Donors.Find(id);
+            return View(donor);
+        }
+        
+        [HttpPost]
+        public IActionResult Edit(Donor d)
+        {
+            if (d.LastDonationDate >= DateOnly.FromDateTime(DateTime.Today))
+            {
+                ModelState.AddModelError("LastDonationDate", "The last donation date must be a past date.");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Donors.Update(d);
+                var res = db.SaveChanges();
+                if (res > 0)
+                {
+                    return RedirectToAction("List");
+                }
+            }
+            return View(d);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var donor = db.Donors.Find(id);
+            return View(donor);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Donor d)
+        {
+            db.Donors.Remove(d);
+            var res = db.SaveChanges();
+            if (res > 0)
+            {
+                return RedirectToAction("List");
+            }
+            return View(d);
+        }
+
+        public IActionResult List()
+        {
+            var data = db.Donors.ToList();
+            return View(data);
+        }
+
+        public IActionResult Filter(string bloodGroup)
+        {
+            ViewBag.SelectedGroup = bloodGroup;
+            List<Donor> donors;
+
+            if (string.IsNullOrEmpty(bloodGroup))
+            {
+                donors = (from item in db.Donors
+                         select item ).ToList();
+            }
+            else
+            {
+                donors = (from item in db.Donors
+                         where item.BloodGroup == bloodGroup
+                         select item ).ToList();
+            }
+            return View(donors);
+        }
+
+        public IActionResult Sort()
+        {
+            var data = ( from item in db.Donors
+                                orderby item.LastDonationDate descending
+                                select item).ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public IActionResult Count()
+        {
+            var donors = (from d in db.Donors.Include(item => item.Donations)
+                          select d).ToList();
+
+            return View(donors);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
